3c1782a507b37d5bf02f71f785b62f406c39b4fb
marcogarganigo <marcogarga11@gmail.com>
2024-11-05 14:01:29 +0100

    login

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,19 +1,22 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"PT Sans"', 'sans-serif']
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        bixcolor: {
+          default: "#074048",
+          light: "#006664",
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
